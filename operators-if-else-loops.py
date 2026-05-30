18ad837bbfaa496e8a5819b6d70019b1a23c5975
anup-codes <anup2812007@gmail.com>
2026-05-30 20:41:10 +0530

    task 2

--- operators-if-else-loops.py
+++ operators-if-else-loops.py
@@ -164,3 +164,186 @@ while num>0:
 print(rev)
 
 
+# Problem 8: Take a user input as integer N. Find out the sum from 1 to N. If any number if divisible by 5, then skip that number. And if the sum is greater than 300, don't need to calculate the sum further more. Print the final result. And don't use for loop to solve this problem.
+# Example 1:
+
+# Input:
+
+# 30
+# Output:
+
+# 276
+
+N = int(input("enter number"))
+sum = 0
+i = 1
+while i< N+1 :
+  if i % 5 == 0:
+    i+= 1
+    continue
+    
+  
+  sum = sum + i
+  if sum > 300:
+    sum = sum -i
+    break
+  
+  i+=1
+
+print(sum)
+
+# Problem 9: Write a program that keeps on accepting a number from the user until the user enters Zero. Display the sum and average of all the numbers.
+
+n = int(input("enter number  "))
+
+sum = 0
+count=1
+
+while n>= 0:
+  if n ==0: 
+    break
+
+  sum += n 
+  
+  avg = sum/ count
+  count+=1
+  n = int(input("enter next  number  "))
+  
+print(sum)
+print (avg)
+
+
+# Problem 9: Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5, between 2000 and 3200 (both included). The numbers obtained should be printed in a comma-separated sequence on a single line.
+
+for i in range (2000, 3001):
+  if i % 7 ==0 and i % 5 !=0 :
+    print(i ,end=',')
+
+
+
+
+# Problem 10: Write a program, which will find all such numbers between 1000 and 3000 (both included) such that each digit of the number is an even number. The numbers obtained should be printed in a space-separated sequence on a single line.
+
+
+
+
+
+
+
+
+
+
+# Problem 11: A robot moves in a plane starting from the original point (0,0). The robot can move toward UP, DOWN, LEFT and RIGHT with a given steps.
+# The trace of robot movement is shown as the following:
+
+# UP 5
+# DOWN 3
+# LEFT 3
+# RIGHT 2
+# !
+# The numbers after the direction are steps.
+
+# ! means robot stop there.
+
+# Please write a program to compute the distance from current position after a sequence of movement and original point.
+
+# If the distance is a float, then just print the nearest integer.
+
+# Example:
+
+# Input:
+
+# UP 5
+# DOWN 3
+# LEFT 3
+# RIGHT 2
+# !
+# Output:
+
+# 2
+
+pos = [0,0]
+
+while True:
+  s = input("enter the direction with the step ")
+  if s == '!':
+    break
+  dir = s.split()[0]
+  steps = int(s.split()[1])
+
+  if dir =='UP':
+    pos[1] += steps
+  elif dir  =='DOWN':
+    pos[1] -= steps
+  elif dir == 'LEFT':
+    pos[0]  -= steps
+  elif dir =='RIGHT':
+    pos[0]+= steps
+  else:
+    pass
+
+print((pos[0]**2 + pos[1]**2)**0.5)
+
+
+# Problem 12:Write a program to print whether a given number is a prime number or not
+
+num = int(input("enter the number"))
+count = 0
+
+for i in range (2,num//2):
+  if num % i == 0:
+    count += 1
+
+if count ==0 :
+  print ( "it is a prime no.")
+else :
+  print ("it is not a prime no.")
+ 
+
+
+# Problem 13:Print all the Armstrong numbers in a given range.
+# Range will be provided by the user
+# Armstrong number is a number that is equal to the sum of cubes of its digits. For example 0, 1, 153, 370, 371 and 407 are the Armstrong numbers.
+
+start = int(input("Enter starting range: "))
+end = int(input("Enter ending range: "))
+
+print("Armstrong numbers are:")
+
+for num in range(start, end + 1):
+    temp = num
+    sum = 0
+
+    while temp > 0:
+        digit = temp % 10
+        sum += digit ** 3
+        temp //= 10
+
+    if sum == num:
+        print(num)
+
+
+
+
+# Problem 14:Calculate the angle between the hour hand and minute hand.
+# Note: There can be two angles between hands; we need to print a minimum of two. Also, we need to print the floor of the final result angle. For example, if the final angle is 10.61, we need to print 10.
+
+# Input:
+# H = 9 , M = 0
+# Output:
+# 90
+# Explanation:
+# The minimum angle between hour and minute hand when the time is 9 is 90 degress.
+
+import math
+
+H = int(input("Enter hour: "))
+M = int(input("Enter minute: "))
+
+hour_angle = 30 * H + 0.5 * M
+minute_angle = 6 * M
+
+angle = abs(hour_angle - minute_angle)
+angle = min(angle, 360 - angle)
+
+print(math.floor(angle))
