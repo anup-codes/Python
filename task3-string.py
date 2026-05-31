83d6d40c42416f06127729ec5ca20cf8297ca6cd
anup-codes <anup2812007@gmail.com>
2026-05-31 23:48:07 +0530

    till problem 4

--- task3-string.py
+++ task3-string.py
@@ -12,4 +12,53 @@ for i in range(0,n):
   print()
 
 
-  
+# Problem 2: Print the following pattern.
+# * 
+# * * 
+# * * * 
+# * * * * 
+# * * * * * 
+# * * * * 
+# * * * 
+# * * 
+# *
+
+n = 8
+for i in range(0,n+1):
+  for j in range(0,i):
+    print("*",end=" ")
+  print()
+
+for i in range(0,n):
+  for j in range(n-i-1,0,-1):
+   print("*",end=" ")
+  print()
+
+  # Problem 3:Write a program to pring the following pattern
+#     *
+#   * * *
+# * * * * *
+n= 4
+
+for i in range (1,n+1):
+ print(" "*n,end="")
+ print("* "*i)
+ n = n-1
+
+
+ # Problem 4:Write a program to print the following pattern
+# 1
+
+# 2 1
+
+# 3 2 1
+
+# 4 3 2 1
+
+# 5 4 3 2 1
+
+n = 5
+for i in range (1,n+1):
+  for j in range(i,0,-1):
+    print(j,end='')
+  print()
