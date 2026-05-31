e7fde7aedb3ecea9788bb61bd5dd362c60f5dd63
anup-codes <anup2812007@gmail.com>
2026-05-31 20:15:07 +0530

    problem 1

---
+++ task3-string.py
@@ -0,0 +1,15 @@
+# Problem 1 - Print the following pattern. Write a program to use for loop to print the following reverse number pattern.
+# 5 4 3 2 1 
+# 4 3 2 1 
+# 3 2 1 
+# 2 1 
+# 1
+
+n = 5
+for i in range(0,n):
+  for j in range(n-i,0,-1):
+    print(j,end=" ")
+  print()
+
+
+  
