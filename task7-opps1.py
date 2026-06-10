d873eb3001c2ccff37bbc1df91c15799c62756c5
anup-codes <anup2812007@gmail.com>
2026-06-10 11:49:16 +0530

    problem1 done

---
+++ task7-opps1.py
@@ -0,0 +1,46 @@
+'''
+Q-1: Rectangle Class
+Write a Rectangle class in Python language, allowing you to build a rectangle with length and width attributes.
+
+Create a Perimeter() method to calculate the perimeter of the rectangle and a Area() method to calculate the area of ​​the rectangle.
+
+Create a method display() that display the length, width, perimeter and area of an object created using an instantiation on rectangle class.
+
+Eg. After making above classes and methods, on executing below code:-
+
+my_rectangle = Rectangle(3 , 4)
+my_rectangle.display()
+Output:
+
+The length of rectangle is:  3
+The width of rectangle is:  4
+The perimeter of rectangle is:  14
+The area of rectangle is:  12 
+'''
+
+
+
+class Rectangle :
+  def __init__(self,x,y):
+    self.length = x
+    self.width = y
+    self.display()
+
+  def display(self):
+    print("The length of rectangle is:",self.length)
+    print("The width of rectangle is: ", self.width)
+    print("The perimeter of rectangle is: ", self.parameter())
+    print("The area of rectangle is: ", self.area())
+   
+
+  def parameter(self):
+    parameter = 2*(self.length + self.width)
+    return parameter
+  
+  def area (self):
+    area = self.length*self.width
+    return area
+  
+
+rec = Rectangle(4,5)
+rec.display
